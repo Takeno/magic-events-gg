03f6c9475480417cecbb12335fbbe44287c05399
Matteo Manchi <matteo.manchi@gmail.com>
2022-01-29 18:12:28 +0100

    chore: styling login and signup pages

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,7 @@
 import {useState} from 'react';
 import type {NextPage} from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
 import {useRouter} from 'next/router';
 import {useUser} from '../contexts/UserContext';
 
@@ -27,26 +28,61 @@ const Login: NextPage<PageProps> = () => {
         <title>Tutti gli eventi di Magic vicino a te! - magic-events.gg</title>
       </Head>
 
-      <h1>Login</h1>
+      <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full space-y-8">
+          <div>
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+              {"Effettua l'accesso"}
+            </h2>
+            <p className="mt-2 text-center text-sm text-gray-600">
+              Hai già un account?{' '}
+              <Link href="/signup">
+                <a className="font-medium text-indigo-600 hover:text-indigo-500">
+                  Registrati ora!
+                </a>
+              </Link>
+            </p>
+          </div>
+          <form className="mt-8 space-y-6" action="#" onSubmit={handleSubmit}>
+            <div className="rounded-md shadow-sm -space-y-px">
+              <label htmlFor="email-address" className="sr-only">
+                Email
+              </label>
+              <input
+                id="email-address"
+                type="email"
+                autoComplete="email"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
 
-      <form onSubmit={handleSubmit}>
-        <input
-          placeholder="email"
-          name="email"
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          placeholder="password"
-          name="password"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+              <input
+                id="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
 
-        <button>Login</button>
-      </form>
+            <button
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Accedi
+            </button>
+          </form>
+        </div>
+      </div>
     </>
   );
 };

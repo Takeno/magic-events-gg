48e31d3f874634eea04a8c5f15e6102e44e81ff5
Matteo Manchi <matteo.manchi@gmail.com>
2022-02-19 17:18:42 +0100

    chore: add Google Tag Manager

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type {AppProps} from 'next/app';
 import {useRouter} from 'next/router';
+import Script from 'next/script';
 import {PropsWithChildren} from 'react';
 import Footer from '../components/Layout/Footer';
 import Header from '../components/Layout/Header';
@@ -19,6 +20,12 @@ function MyApp({Component, pageProps}: AppProps) {
         </div>
         <Footer />
       </main>
+
+      {process.env.NEXT_PUBLIC_GTAG && (
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTAG}');`}
+        </Script>
+      )}
     </UserProvider>
   );
 }

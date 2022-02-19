d3c876bb70fffbf93af52031a1d60518d44d18dd
Matteo Manchi <matteo.manchi@gmail.com>
2022-02-19 16:12:28 +0100

    feat: custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,27 @@
+import type {NextPage} from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
+import placeholder from '../assets/not-found-placeholder.png';
+
+const NotFoundPage: NextPage<{}> = () => {
+  return (
+    <div className="flex justify-center items-center flex-col py-8">
+      <Image src={placeholder} alt="Not found" aria-hidden="true" />
+
+      <h1 className="text-4xl font-bold text-blue-dark mt-8">
+        Ops, la pagina che cerchi non esiste.
+      </h1>
+      <p>O non è stata ancora sviluppata.</p>
+
+      <p className="mt-4">
+        Se pensi sia un errore, non esitare a{' '}
+        <Link href="/contatti">
+          <a className="underline">contattarci</a>
+        </Link>{' '}
+        per migliorare il servizio.
+      </p>
+    </div>
+  );
+};
+
+export default NotFoundPage;

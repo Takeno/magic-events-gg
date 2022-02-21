c83b7b45104c5cf62aee4ad830b8b2d981dad845
Matteo Manchi <matteo.manchi@gmail.com>
2022-02-21 15:37:10 +0100

    feat: link facebook page in header

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -107,6 +107,21 @@ export default function Header() {
                         Contatti
                       </a>
                     </Link>
+                    <a
+                      href="https://www.facebook.com/magiceventsgg"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-md font-medium"
+                    >
+                      <svg
+                        fill="currentColor"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 30 30"
+                        className="h-6 w-6"
+                      >
+                        <path d="M15,3C8.373,3,3,8.373,3,15c0,6.016,4.432,10.984,10.206,11.852V18.18h-2.969v-3.154h2.969v-2.099c0-3.475,1.693-5,4.581-5 c1.383,0,2.115,0.103,2.461,0.149v2.753h-1.97c-1.226,0-1.654,1.163-1.654,2.473v1.724h3.593L19.73,18.18h-3.106v8.697 C22.481,26.083,27,21.075,27,15C27,8.373,21.627,3,15,3z" />
+                      </svg>
+                    </a>
                   </div>
                 </div>
               </div>
@@ -152,6 +167,15 @@ export default function Header() {
                   Contatti
                 </Disclosure.Button>
               </Link>
+              <Disclosure.Button
+                as="a"
+                href="https://www.facebook.com/magiceventsgg"
+                target="_blank"
+                rel="noreferrer"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              >
+                Seguici su Facebook
+              </Disclosure.Button>
             </div>
           </Disclosure.Panel>
         </>

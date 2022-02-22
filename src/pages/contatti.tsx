6757a358f1939a2ed77017f5f42710f4b92bd177
Matteo Manchi <matteo.manchi@gmail.com>
2022-02-22 11:35:23 +0100

    chore: update contacts texts

--- src/pages/contatti.tsx
+++ src/pages/contatti.tsx
@@ -14,18 +14,24 @@ export default function Contatti() {
         <h1 className="text-3xl font-bold">Contatti</h1>
 
         <p className="mt-2">
-          MagicEvents.gg nasce da un&apos;idea di Matteo Manchi, sviluppato con
-          tecnologie open source e pubblicato su{' '}
-          <a
-            className="underline"
-            href="https://github.com/Takeno/magic-events-gg/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Github
-          </a>
-          .
+          MagicEvents.gg è il primo aggregatore di eventi e tornei di Magic sul
+          territorio studiato per rispondere una volta per tutte alla domanda
+          &quot;dove gioco il mio formato preferito questa sera in questa
+          città?&quot;
         </p>
+
+        <p className="mt-2">
+          L&apos;obiettivo è riuscire ad avvicinare tutti i giocatori, anche i
+          più casual, agli eventi sul territorio (perché Arena è bello, ma
+          tirare bolt in faccia all&apos;avversario ha tutto un altro sapore).
+        </p>
+        <p className="mt-2">
+          Abbiamo in programma tantissime funzionalità dedicate ai giocatori e
+          ai negozianti, e siamo aperti a feedback e suggerimenti per far
+          diventare la piattaforma un punto d&apos;incontro tra giocatori ed
+          organizzatori.
+        </p>
+        <p className="mt-2">Stay safe, wear a mask and play magic!</p>
 
         <h2 className="text-xl font-bold mt-8">Organizzi tornei di Magic?</h2>
         <p className="mt-2">

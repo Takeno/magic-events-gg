c55650cc374f968126ae37666b8a83940a390692
Matteo Manchi <matteo.manchi@gmail.com>
2022-02-26 12:37:05 +0100

    fix: responsive adjustments

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
         <>
           <div className="container mx-auto">
             <div className="relative flex items-center justify-between h-20">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden px-2">
+              <div className="absolute inset-y-0 left-0 flex items-center lg:hidden px-2">
                 <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
@@ -53,9 +53,9 @@ export default function Header() {
                   )}
                 </Disclosure.Button>
               </div>
-              <div className="flex-1 flex items-center justify-end sm:items-stretch sm:justify-between">
+              <div className="flex-1 flex items-center justify-end lg:items-stretch lg:justify-between">
                 <div className="flex-shrink-0 flex items-center">
-                  <span className="sm:absolute translate-y-1 sm:translate-y-9 h-20 sm:h-auto">
+                  <span className="lg:absolute translate-y-1 lg:translate-y-9 h-20 lg:h-auto">
                     <Link href="/">
                       <a>
                         <Image
@@ -71,13 +71,13 @@ export default function Header() {
                   </span>
                   <Link href="/">
                     <a>
-                      <h1 className="text-primary text-xl font-bold sm:pl-24 hidden sm:block">
+                      <h1 className="text-primary text-xl font-bold lg:pl-24 hidden lg:block">
                         magic-events.gg
                       </h1>
                     </a>
                   </Link>
                 </div>
-                <div className="hidden sm:block sm:ml-6 h-full">
+                <div className="hidden lg:block sm:ml-6 h-full">
                   <div className="flex space-x-4">
                     <Link href="/organizzatori">
                       <a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-md font-medium">
@@ -138,7 +138,7 @@ export default function Header() {
             </div>
           </div>
 
-          <Disclosure.Panel className="sm:hidden">
+          <Disclosure.Panel className="lg:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1">
               <Link href="/organizzatori" passHref>
                 <Disclosure.Button
--- src/pages/to/[to].tsx
+++ src/pages/to/[to].tsx
@@ -33,7 +33,7 @@ const SingleTournament: NextPage<PageProps> = ({organizer}) => {
       />
 
       <div className="card max-w-screen-lg mx-auto w-full mt-8 p-4">
-        <div className="flex flex-row justify-between items-center">
+        <div className="flex flex-col-reverse sm:flex-row justify-between items-center">
           <h1 className="text-3xl font-bold">{organizer.name}</h1>
           {organizer.logo && (
             <div className="flex-shrink-0 relative h-24 w-24">
@@ -49,18 +49,30 @@ const SingleTournament: NextPage<PageProps> = ({organizer}) => {
           )}
         </div>
 
-        <div className="flex flex-row justify-between items-center">
+        <div className="flex flex-col sm:flex-row justify-between items-center">
           <a
             href={`https://www.google.com/maps/dir/?api=1&destination=${organizer.location.latitude}%2C${organizer.location.longitude}`}
             target="_blank"
             rel="noreferrer"
             className="hover:underline"
           >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="inline-block h-5 w-5 align-middle"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+                clipRule="evenodd"
+              />
+            </svg>
             {organizer.location.address} - {organizer.location.city} (
             {organizer.location.province}) {organizer.location.country}
           </a>
 
-          <div className="w-24 text-center">
+          <div className="text-center min-w-[6rem]">
             {organizer.facebook && (
               <a
                 href={organizer.facebook}

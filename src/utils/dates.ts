adb37d88406bde408618b9463d7c05c5519e8a4a
Matteo Manchi <matteo.manchi@gmail.com>
2022-12-14 20:11:31 +0100

    fix: adjust server timezone

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,11 +1,16 @@
 import formatDate from 'date-fns/format';
 import add from 'date-fns/add';
+import addMinutes from 'date-fns/addMinutes';
 import it from 'date-fns/locale/it';
 
 type FormatType = typeof formatDate;
 
+const dt = new Date();
+
 export const format: FormatType = function (date, format, options) {
-  return formatDate(date, format, {
+  const fixedTimezone = addMinutes(date, 60 + dt.getTimezoneOffset());
+
+  return formatDate(fixedTimezone, format, {
     locale: it,
     ...options,
   });
